9250f0320b9937a594a235708b616eaad044a6dd
Adam Brewer <adamcbrewer@gmail.com>
2014-01-21 15:11:28 +0000

    Added fonts and first couple of CSS modules in place

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-getbrewer.com
+[getbrewer.com](http://getbrewer.com)
 =============
 
 The design and development practice of Jake and Adam Brewer. Brothers in a digital world.
---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<!--[if IE 8]><html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1,requiresActiveX=true">
+    <meta name="author" content="Adam Brewer">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Get Brewer</title>
+
+    <link type="text/plain" rel="author" href="humans.txt" />
+    <meta name="robots" content="all">
+
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <!-- https://github.com/audreyr/favicon-cheat-sheet -->
+
+    <meta name="description" content="The design and development practice of Jake and Adam Brewer. Brothers in a digital world.">
+    <meta name="keywords" content="studiobrewer, brewerlogic, web design, web development, Jake Brewer, Adam Brewer, web development london, web design london">
+
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content="The design and development practice of Jake and Adam Brewer. Brothers in a digital world."/>
+
+    <link href='http://fonts.googleapis.com/css?family=Karla:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+</head>
+<body data-base-url="">
+
+    <div class="wrapper">
+
+        <section class="content">
+
+            <header class="section">
+                <h1 class="site-title">
+                    <span class="site-title--small">Get</span><br>
+                    Brewer
+                </h1>
+                <i class="dots dots--large"></i>
+
+                <h2 class="contain">The design and development practice of Jake and Adam Brewer. Brothers in a digital world.</h2>
+
+                <nav class="nav">
+                    <a class="nav-item" href="#what-we-do">What We Do</a>
+                    <a class="nav-item" href="#projects">Projects</a>
+                    <a class="nav-item" href="#contact">Contact</a>
+                </nav>
+
+            </header>
+
+            <section id="what-we-do" class="section section--earth">
+                <header>
+                    <h3 class="title">What We Do</h3>
+                    <i class="dots"></i>
+                </header>
+            </section>
+
+            <section id="projects" class="section section--grey">
+                <header>
+                    <h3 class="title">Projects</h3>
+                    <i class="dots"></i>
+                </header>
+            </section>
+
+        </section>
+
+        <footer class="footer">
+            <p class="inner">
+               All content &copy; <?= date('Y'); ?> Jake &amp; Adam Brewer,
+                including respective agencies and clients.
+                Please do not reproduce without consent;
+                we will find you.
+            </p>
+        </footer>
+    </div>
+
+    <script src="assets/js/libs/jquery.js"></script>
+    <script src="assets/js/script.js"></script>
+
+    <script>
+        // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        // ga('create', 'UA-XXXXXX-X');
+        // ga('send', 'pageview');
+    </script>
+
+</body>
+</html>
